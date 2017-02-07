94cdf2c0d0811048d722742f0ae0f3dba2d1cfb7
Mikoaj Panasiuk <mikopanasiuk@o2.pl>
2017-02-07 11:55:21 +0000

    change scene

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -8,3 +8,11 @@ func _ready():
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/levels/level1.tscn")
+
+
+func _on_Button1_pressed():
+	get_tree().change_scene("res://scenes/levels/level4.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://scenes/levels/level5.tscn")
